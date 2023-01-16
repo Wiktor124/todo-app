a520a815cc121332766a828ff510f707ae36aaeb
Víctor Peña <victortkd88@gmail.com>
2023-01-16 16:08:26 -0600

    new properties

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,50 @@
-let todo_list = JSON.parse(localStorage.getItem('todo')) || [];
+// Storage todos
+const todo_list = JSON.parse(localStorage.getItem('todo')) || [];
 
 // Add a todo
 const add_todo = (todo) => {
-  let random_id = Math.floor(Math.random() * 1000);
-  todo_list.push({ id: random_id, complete: false, todo: todo });
+  const todo_id = Math.floor(Math.random() * 1000);
 
+  todo_list.push({id: todo_id, todo: todo, state: false});
   return localStorage.setItem('todo', JSON.stringify(todo_list));
 }
+console.log(todo_list);
 
 // Print todos
-const print_todo = (print_this) => {
-
-  document.getElementById('todo__container').innerHTML = print_this.map(item => {
-    return `<li><button id="complete">Complete</button><span>${item.todo}</span><button id="delete" value="${item.id}">Delete</button></li>`
+const print_todo = () => {
+  document.getElementById('todo__container').innerHTML = todo_list.map(item => {
+    return `
+      <li><span>${item.todo}</span><button id="delete" value="${item.id}">Delete</button></li>
+    `
   }).join('');
 }
-print_todo(todo_list);
+print_todo();
 
-//Delete a todo
-const delete_todo = (button) => {
-  const index_todo = todo_list.findIndex(index => index.id === parseInt(button.value));
+// Delete a todo
+const delete_todo = (delete_this) => {
+  const index_todo = todo_list.findIndex(index => index.id === parseInt(delete_this.value));
+  console.log(index_todo);
 
-  todo_list.splice(index_todo, 1);
-  localStorage.setItem('todo', JSON.stringify(todo_list));
+  todo_list.splice(index_todo, 1)
+  return localStorage.setItem('todo', JSON.stringify(todo_list))
 }
 
 document.querySelector('.delete').addEventListener('click', (e) => {
-  if (e.target.matches('#delete')) {
-    delete_todo(e.target);
-    console.log(e.target);
+
+  if(e.target.matches('#delete')) {
+    delete_todo(e.target)
+    // console.log(e.target);
   }
   return print_todo(todo_list);
-});
+})
 
+
+// Call functions
 const form = document.getElementById('todo__form');
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
   add_todo(document.getElementById('todo').value);
-  print_todo(todo_list);
-  form.reset();
-});
+  print_todo();
+  form.reset()
+})
