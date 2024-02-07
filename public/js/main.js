6ccb9e4e8d8674221a956d33a676b3f6c5c36b20
Víctor Peña <victortkd88@gmail.com>
2024-02-07 13:34:41 -0600

    new features

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,56 @@
-// Storage todos
-const todo_list = JSON.parse(localStorage.getItem('todo')) || []
-console.log(todo_list.length)
+const todo_list = JSON.parse(localStorage.getItem('todo')) || [];
 
 const date = new Date();
 
+const currentDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 const title = document.querySelector('.header__date')
-title.textContent = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+title.textContent = currentDate;
+
+const heightAppointContainer = document.getElementById('add-appointment-container').offsetHeight;
+const todoContainer = document.getElementById('todo__container');
+
+todoContainer.style.height = `calc(100vh - ${heightAppointContainer + 10}px)`;
 
 // Add a todo
 const add_todo = (todo) => {
   const todo_id = crypto.randomUUID()
 
-  todo_list.push({ id: todo_id, todo: todo, state: false })
+  todo_list.push({ ...todo, id: todo_id, state: false })
   localStorage.setItem('todo', JSON.stringify(todo_list))
 }
 
 // Print todos
 const print_todo = () => {
-  document.getElementById('todo__container').innerHTML = todo_list
-    .map((item) => {
+  todoContainer.innerHTML = todo_list
+    .map((cita) => {
+      const { id, appointment, dateAppointment, timeAppointment } = cita;
+      
       return `
-      <li><span>${item.todo}</span><button type="button" id="delete" value="${item.id}"></button></li>
+      <li>
+        <div>
+          <div><strong>Fecha:</strong> ${!dateAppointment ? 'Sin establecer' : dateAppointment}</div></span>
+          <div><strong>Hora:</strong> ${!timeAppointment ? 'Sin establecer' : timeAppointment }</div>
+        </div>
+        
+        <div class="todo__desc">
+          <p id="${id}">${appointment}</p>
+        </div>
+        <div class="buttons-container">
+          <button type="button" class="edit-btn" data-id="${id}"></button>
+          <button type="button" id="delete" value="${id}"></button>
+        </div>
+      </li>
       `
     })
     .join('')
 }
-print_todo() //
+print_todo()
 
-// Delete a todo
-const delete_todo = (delete_this) => {
+// Delete a Appointment
+const deleteAppointment = (delete_this) => {
   const index_todo = todo_list.findIndex(
     (index) => index.id === delete_this.value,
   )
-  // console.log(index_todo)
 
   todo_list.splice(index_todo, 1)
   localStorage.setItem('todo', JSON.stringify(todo_list))
@@ -40,17 +58,46 @@ const delete_todo = (delete_this) => {
 
 document.querySelector('.delete').addEventListener('click', (e) => {
   if (e.target.matches('#delete')) {
-    delete_todo(e.target)
-    // console.log(e.target)
+    const confirmDeleleting = confirm('¿Deseas eliminar esta Cita?');
+
+    if(!confirmDeleleting) return;
+    
+    deleteAppointment(e.target)
   }
   return print_todo(todo_list)
 })
+
+// Edit Appointment
+const editConatiner = document.querySelector('.todo__container');
+editConatiner.addEventListener('click', ({ target }) => {
+  
+  if(!target.matches('.edit-btn')) return;
+  const newDataAppointment = prompt('hello');
+
+  if(newDataAppointment === null || !newDataAppointment) return;
+  
+  const indexAppointment = todo_list.find(appointment => appointment.id === target.dataset.id);
+  indexAppointment.appointment = newDataAppointment;
+  
+  localStorage.setItem('todo', JSON.stringify(todo_list))
+  return print_todo(todo_list)
+})
+
 
 // Call functions
 const form = document.getElementById('todo__form')
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  add_todo(document.getElementById('todo').value)
+
+  const appointment = new FormData(form).get('appointment')
+  const dateAppointment = new FormData(form).get('date-appointment');
+  const timeAppointment = new FormData(form).get('time-appointment');
+
+  add_todo({
+    appointment,
+    dateAppointment,
+    timeAppointment
+  })
   print_todo()
   form.reset()
 })
